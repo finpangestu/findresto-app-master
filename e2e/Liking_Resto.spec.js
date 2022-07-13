c27c885d579a85e255b74c957746d53497414208
finpangestu <195410015@students.akakom.ac.id>
2022-07-13 16:23:30 +0700

    add E2E Testing

---
+++ e2e/Liking_Resto.spec.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+
+Feature('Liking Resto');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+const firstCondition = 'Belum ada Restoran pilihan yang anda sukai.';
+
+Scenario('showing empty favorite restaurant', ({ I }) => {
+  I.seeElement('#list-rest');
+  I.see(firstCondition, '#list-rest');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see(firstCondition, '#list-rest');
+
+  // Like
+  I.amOnPage('/');
+
+  I.waitForElement('.list_item_title a', 10);
+  I.seeElement('.list_item_title a');
+  const firstItem = locate('.list_item_title a').first();
+  const firstItemTitle = await I.grabTextFrom(firstItem);
+  I.click(firstItem);
+
+  I.waitForElement('#likeButton i', 10);
+  I.seeElement('#likeButton i');
+  I.click('#likeButton i');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list_item');
+  const likedItemTitle = await I.grabTextFrom('.list_item_title');
+
+  assert.strictEqual(firstItemTitle, likedItemTitle);
+});
+
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.see(firstCondition, '#list-rest');
+
+  // Like
+  I.amOnPage('/');
+
+  I.waitForElement('.list_item_title a', 10);
+  I.seeElement('.list_item_title a');
+  const firstItem = locate('.list_item_title a').first();
+  const firstItemTitle = await I.grabTextFrom(firstItem);
+  I.click(firstItem);
+
+  I.waitForElement('#likeButton i', 10);
+  I.seeElement('#likeButton i');
+  I.click('#likeButton i');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list_item');
+  const likedItemTitle = await I.grabTextFrom('.list_item_title');
+
+  assert.strictEqual(firstItemTitle, likedItemTitle);
+
+  // Unlike
+  I.click(likedItemTitle);
+
+  I.waitForElement('#likeButton i', 10);
+  I.seeElement('#likeButton i');
+  I.click('#likeButton i');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#list-rest');
+  const unlikedItemTitle = await I.grabTextFrom('#list-rest');
+
+  assert.strictEqual(unlikedItemTitle, firstCondition);
+});
